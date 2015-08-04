0f29536c2db402b5b0a8854e7e589a33bda299f7
LeaYeh <smile2140@gmail.com>
2015-08-04 22:06:14 +0800

    test

--- prototype.c
+++ prototype.c
@@ -1,7 +1,17 @@
 #include "golbal.h"
 
+void DatasetRead (char *filename, const int &dataNum, const int &dimNum);
+
 int main (int argc, char *argv[]) {
   float pa, pb;
+  int dataPointSize, dataDimension;
+
+  DatasetRead('a', dataPointSize, dataDimension);  
 
   return 0;
+}
+
+void DatasetRead (char *filename, const int &dataNum, const int &dimNum) {
+  dataNum = 1;
+
 }
