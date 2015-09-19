15dc1c8b71bc845be2119cd1dced3ae60af0b202
LeaYeh <smile2140@gmail.com>
2015-09-19 20:39:59 +0800

    add [func]setInit_3d to setup necessary value

--- prototype.c
+++ prototype.c
@@ -1,7 +1,12 @@
 #include "global.h"
+float pa, pb;
+float avg_x, avg_y, avg_z;
+float avg_xy, avg_xz, avg_yz, avg_xyz;
 
 void DatasetRead(char *filePath, float *point);
 void PrintDataPoint(float *points);
+void setInit_3d(float *points);
+
 int main (int argc, char *argv[]) {
   // filePath need able to user input 
   char *filePath = "dataset/sipu_dataset/dim3.txt";
@@ -20,7 +25,8 @@ int main (int argc, char *argv[]) {
   }*/
   DatasetRead(filePath, points);
   PrintDataPoint(points);
-  //printf("%5.2f ", *(points));
+  setInit_3d(points);
+
   return 0;
 }
 void PrintDataPoint(float *points) {
@@ -33,6 +39,34 @@ void PrintDataPoint(float *points) {
     printf("\n");
   }
   printf("[finish] print dataset.\n");
+}
+void setInit_3d(float *points) {
+  float sum_x = 0, sum_y = 0, sum_z = 0;
+  float sum_xy = 0, sum_xz = 0, sum_yz = 0;
+  float sum_xyz = 0;
+  int i, j;
+
+  for (i = 0; i < dataPointSize*dataDimension; i += dataDimension) {
+    //if (i % dataDimension == 0) {
+      sum_x += points[i];
+      sum_y += points[i+1];
+      sum_z += points[i+2];
+      sum_xy += points[i] * points[i+1];
+      sum_xz += points[i] * points[i+2];
+      sum_yz += points[i+1] * points[i+2];
+      sum_xyz += points[i] * points[i+1] * points[i+2];
+    //} 
+  }
+  avg_x = (float)(sum_x / dataPointSize);
+  avg_y = sum_y / dataPointSize;
+  avg_z = sum_z / dataPointSize;
+  avg_xy = sum_xy / dataPointSize;
+  avg_xz = sum_xz / dataPointSize;
+  avg_yz = sum_yz / dataPointSize;
+  avg_xyz = sum_xyz / dataPointSize;
+  printf("avg_x = %5.2f, avg_y = %5.2f, avg_z = %5.2f\n", avg_x, avg_y, avg_z);
+  printf("avg_xy = %5.2f, avg_xz = %5.2f, avg_yz = %5.2f\n", avg_xy, avg_yz, avg_xz);
+  printf("avg_xyz = %5.2f\n", avg_xyz);
 }
 void DatasetRead(char *filePath, float *point) {
   FILE *fp = NULL;
@@ -50,8 +84,7 @@ void DatasetRead(char *filePath, float *point) {
       if (strlen(pch) > 0) { 
         //printf("%.2f\n", atof(pch));
         //point[data_index++] = atof(pch);
-        point[data_index] = atof(pch);
-        data_index++;
+        point[data_index++] = atof(pch);
         if (data_index == dataPointSize*dataDimension) {
           fclose(fp);
           printf("[success] read dataset.\n");
