981d8f7540f38cb5ec679a7b902d49f1c36452ef
LeaYeh <smile2140@gmail.com>
2016-04-07 00:40:22 +0800

    [record merge_split.py] init

--- py/merge_split.py
+++ py/merge_split.py
@@ -71,13 +71,6 @@ class Tree:
     if method == 'rwm':
       cluster1, cluster2, in_boundary, coeff = rwm.rwm_cut(self.datapoints)
       bound_size = len(in_boundary[0]) + len(in_boundary[1])
-      # if self.in_bound_record:
-      #   rec_cut_id = self.in_bound_record[0]
-      #   rec_points = self.in_bound_record[1]
-      #   rec_color  = self.in_bound_record[2]
-      #   b_left, b_right = rwm.cut_by_coeff(rec_points, coeff)
-      #   self.left.in_bound_record.append((rec_cut_id, b_left, rec_color))
-      #   self.right.in_bound_record.append((rec_cut_id, b_right, rec_color))
 
       # create node by split result
       self.right = Tree(self, cluster1)
@@ -85,13 +78,26 @@ class Tree:
       self.left = Tree(self, cluster2)
       self.left.size = len(cluster2)
 
-      # if bound_size > 0:
-      #   self.right.in_bound_record.append((Tree.total_cut, in_boundary[0], '+'))
-      #   self.left.in_bound_record.append((Tree.total_cut, in_boundary[1], '-'))
-
+      print("cid = ", Tree.total_cut, ", bound_size = ", bound_size, "self.size/6 = ", self.size // 5)
       if bound_size >= (self.size // 5):
         self.left.bad_cut = self.bad_cut + 1
         self.right.bad_cut = self.bad_cut + 1
+        print("cut id ", Tree.total_cut, " is bad cut")
+        self.left.in_bound_record.append((Tree.total_cut, '-', in_boundary[0]))
+        self.right.in_bound_record.append((Tree.total_cut, '+', in_boundary[1]))
+
+      if self.in_bound_record:
+        print("self.in_bound_record = ", len(self.in_bound_record))
+        print("node addr = ", self)
+      for bound_rec in self.in_bound_record:
+        rec_cut_id = bound_rec[0]
+        rec_color  = bound_rec[1]
+        rec_points = bound_rec[2]
+        b_left, b_right = rwm.cut_by_coeff(rec_points, coeff)
+        self.left.in_bound_record.append((rec_cut_id, rec_color, b_left))
+        self.right.in_bound_record.append((rec_cut_id, rec_color, b_right))
+        print("cur cid = {}, rec_cut_id = {}".format(Tree.total_cut, rec_cut_id))
+
 
       print("after split, child.bad_cut = {}".format(self.left.bad_cut))
       Tree.total_cut += 1
@@ -99,19 +105,16 @@ class Tree:
     return
 
 
-  def find_merge_candidate(root):
-    candidates = []
-    def _get_cands(node, candidates):
-      if node is None:
-        return
-      if node.in_bound_record and node.grounded and node.active:
-        print("node.parent = {}, #in_bound_points = {}".format(node.parent, node.in_bound_record[0]))
-        candidates.append(node)
-        return
-      _get_cands(node.left, candidates)
-      _get_cands(node.right, candidates)
+  def _is_all_leves_grounded():
+    return
 
-    _get_cands(root, candidates)
+
+  def find_merge_candidate(root, grounded_list):
+    candidates = []
+
+    for node in grounded_list:
+      if node.in_bound_record:
+        candidates.append(node)
 
     return candidates
 
@@ -131,21 +134,26 @@ class Tree:
     final_clusters = []
 
     while grounded_nodes:
-      cur_node = grounded_nodes.pop(0)
+      cur_node = grounded_nodes[0]
       cur_recs = cur_node.in_bound_record
 
-
-      print("cur_recs = ", cur_recs)
       if not cur_node.active:
+        grounded_nodes.pop(0)
         continue
       if not cur_recs:
+        grounded_nodes.pop(0)
         final_clusters.append(cur_node)
       else:
-        for other_node in grounded_nodes:
-          if not other_node.active:
-            continue
+        other_size = len(grounded_nodes[1:])
+        other_nonactive = 0
+
+        for other_node in grounded_nodes[1:]:
           need_merge = False
           other_recs = other_node.in_bound_record
+
+          if not other_node.active:
+            other_nonactive += 1
+            continue
           if other_recs:
             for crec in cur_recs:
               cur_cut_id = crec[0]
@@ -158,12 +166,17 @@ class Tree:
                 if (cur_cut_id == other_cut_id) and (cur_color != other_color):
                   need_merge = True
           if need_merge:
-            np.append(cur_node.datapoints, other_node.datapoints, axis=0)
+            # print("\n\n================ before\n", cur_node.datapoints, "\n-----------\n", other_node.datapoints)
+            cur_node.datapoints = np.append(cur_node.datapoints, other_node.datapoints, axis=0)
+            # print("================after\n", cur_node.datapoints)
             cur_node.in_bound_record.extend(other_recs)
             other_node.active = False
-        final_clusters.append(cur_node)
-
-    print("final_clusters = ", len(final_clusters))
+          else:
+            grounded_nodes.pop(0)
+            final_clusters.append(cur_node)
+        if other_nonactive == other_size:
+          grounded_nodes.pop(0)
+          final_clusters.append(cur_node)
 
     return final_clusters
 
@@ -214,10 +227,10 @@ def paint_tree(current_node, root_node, deep=0):
     raise ValueError(msg)
 
   print('    ' * deep, current_node.grounded)
-  if current_node.left is None:
+  if (current_node.left and current_node.right) is None:
     return 
-  if current_node.grounded:
-    return
+  # if current_node.grounded:
+  #   return
   # if current_node.active is False:
   #   return
 
@@ -254,12 +267,52 @@ if __name__ == '__main__':
   points, label = utl.read_from_text('2d5c_std')
 
   ms_tree = ms2c(points)
-  paint_tree(ms_tree, ms_tree)
+  # paint_tree(ms_tree, ms_tree)
 
   grounded_list = []
   Tree.get_grounded_node(ms_tree, grounded_list)
 
-  print("\n\n============================")
-  for node in grounded_list:
+  ms_cands = Tree.find_merge_candidate(ms_tree, grounded_list)
+
+
+  print("\n#grounded = ", len(grounded_list))
+  for g in grounded_list:
+    print(g, " , level = ", g.level)
+  gnodes = [node.datapoints for node in grounded_list]
+  gnode_bounds = [node.in_bound_record for node in grounded_list if node.in_bound_record]
+
+  i = 0
+  for node in gnode_bounds:
+    print("node", i)
+    for rec in node:
+      print("   cid = {}, side = {}".format(rec[0], rec[1]))
+    i += 1
+
+  print("\n#m_candidates = ", len(ms_cands))
+  for c in ms_cands:
+    print(c, " , level = ", c.level)
+
+
+  # for g in grounded_list:
+  #   print("g node: ", g.in_bound_record)
+  #
+
+
+  # utl.draw_clusters(gnodes)
+
+  mnodes = [node.datapoints for node in ms_cands]
+  # print("mnodes = \n", mnodes)
+
+  final_node = Tree.merge(grounded_list)
+  final_cls = [x.datapoints for x in final_node]
+
+  # for node in final_node:
+  #   print(node)
+
+  print("#final_cls = ", len(final_cls))
+  utl.draw_clusters(final_cls)
+
+  # print("\n\n============================")
+  # for node in grounded_list:
     # print("cut_id = {}, ")
 
