166031f98ff9c7ec23a6a669c8510883c4f1a92c
LeaYeh <smile2140@gmail.com>
2016-04-11 00:28:10 +0800

    [update rwm.py] fix bug about rwm should be sqrt

--- py/rwm.py
+++ py/rwm.py
@@ -1,6 +1,9 @@
 import utils as utl
 np = utl.np
 plt = utl.plt
+import tcf as tcf
+
+
 
 """
 input:  datapoints
@@ -10,7 +13,8 @@ def _rwm(datapoints):
   size, dim = datapoints.shape
   center = np.mean(datapoints, axis=0)
 
-  r = np.sum((datapoints - center) ** 2, axis=1)
+  r = np.sqrt(np.sum((datapoints - center) ** 2, axis=1))
+  # r = np.sum((datapoints - center) ** 2, axis=1)
   R = sum(r)
   w = r.dot(datapoints) / R
   v = w - center
@@ -19,36 +23,78 @@ def _rwm(datapoints):
   return coeff
 
 
+def cut_by_coeff(datapoints, coeff):
+  c_left = []
+  c_right = []
+  unit_len = sum(coeff[:-1] ** 2) ** 0.5
+
+  for point in datapoints:
+    p2b_dist = (sum(point * coeff[:-1]) + coeff[-1]) / unit_len
+    if p2b_dist >= 0:
+      c_right.append(point)
+    else:
+      c_left.append(point)
+  c_left = np.array(c_left, np.float)
+  c_right = np.array(c_right, np.float)
+
+  return (c_left, c_right)
+
+
 """
 input:  datapoints(numpy.ndarray)
-output: list
+output: two list of data in each cluster
 """
-def rwm_cut(datapoints):
+@utl.log_msg
+def rwm_cut(datapoints, boundary_width=10):
+  in_boundary = 0
   size, dim = datapoints.shape
   c_left = []
   c_right = []
 
   coeff = _rwm(datapoints)
 
+  r_bp = []
+  l_bp = []
+
   for point in datapoints:
-    if sum(point * coeff[:-1]) + coeff[-1] >= 0:
+    # calc distance from point to boundary
+    unit_len = sum(coeff[:-1] ** 2) ** 0.5
+    p2b_dist = (sum(point * coeff[:-1]) + coeff[-1]) / unit_len
+    if abs(p2b_dist) <= boundary_width:
+      if p2b_dist >= 0:
+        r_bp.append(point)
+      else:
+        l_bp.append(point)
+    if p2b_dist >= 0:
       c_right.append(point)
     else:
       c_left.append(point)
 
-  return np.array(c_left, np.float), np.array(c_right, np.float)
+  c_left = np.array(c_left, np.float)
+  c_right = np.array(c_right, np.float)
+  r_bp = np.array(r_bp, np.float)
+  l_bp = np.array(l_bp, np.float)
+
+  # left, right, in boundary point, coeff
+  return c_left, c_right, (r_bp, l_bp), coeff
 
 
 
 if __name__ == '__main__':
-  points, label = utl.gaussian_data_generator(dim=2, cls=2)
- 
-  for i in np.unique(label):
-    fetch_cluster = points[label == i]
-    plt.scatter(fetch_cluster[:, 0], fetch_cluster[:, 1], color=np.random.rand(3));
+  # for i in range(2, 256):
+  #   points, label = utl.gaussian_data_generator(dim=i, objs_size=[1000, 1000, 1000, 1000, 1000], cls=5)
+  #   print("dim = ", i)
+  #   rwm_cut(points)
+  points, label = utl.gaussian_data_generator(dim=2, cls=7)
+  # rwm_cut(points)
 
-  coeff = rwm(points)
-  utl.graph(coeff, range(-1000, 1000))
+  # for i in np.unique(label):
+  #   fetch_cluster = points[label == i]
+  #   plt.scatter(fetch_cluster[:, 0], fetch_cluster[:, 1], color=np.random.rand(3));
+
+
+  c1, c2, d = rwm_cut(points)
+
 
   plt.show()
 
