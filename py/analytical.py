007bea737618b28da031f98a8e848978be5be891
LeaYeh <smile2140@gmail.com>
2016-04-12 03:48:43 +0800

    [update analytical.py] add doctest in part of functions

--- py/analytical.py
+++ py/analytical.py
@@ -1,4 +1,5 @@
 import utils as utl
+import doctest
 np = utl.np
 plt = utl.plt
 
@@ -21,6 +22,18 @@ def build_table(dim_range):
 
 
 def _dec2bin(dec, dim):
+  '''
+  :input: dec, dim
+  :output: binary
+  >>> _dec2bin(5, 3)
+  array([1, 0, 1])
+  >>> _dec2bin(5, 5)
+  array([1, 0, 1, 0, 0])
+  >>> _dec2bin(17, 5) 
+  array([1, 0, 0, 0, 1])
+  >>> _dec2bin(17, 7) 
+  array([1, 0, 0, 0, 1, 0, 0])
+  '''
   res = np.zeros(dim, np.int)
 
   for i in range(dim - 1, -1, -1):  
@@ -34,9 +47,19 @@ def _dec2bin(dec, dim):
 
 
 def _bin2dec(bools):
+  '''
+  :input: ndarray
+  :output: dec
+  >>> _bin2dec([0, 0, 1, 1, 0, 1, 0])
+  44
+  >>> _bin2dec([1, 1, 0, 1, 0])
+  11
+  '''
+
   res = 0
 
   for i in range(0, len(bools)):
+    # if bools[i] > 0:
     if bools[i]:
       res += 2 ** i
 
@@ -44,10 +67,23 @@ def _bin2dec(bools):
 
 
 def _box_vote(datapoints):
+  ''' check point's quadrant then vote it, then find the first and second boxes
+  :input: datapoints <- ndarray
+  :output: first and second boxes id <- (int, int)
+
+  1, 3, 3, 0, 0, 3
+  >>> _box_vote(np.array([[2, -1], [4, 2], [0, 2], [-2, -10], [-2, -1], [33, 2]]))
+  (3, 0)
+
+  5, 2, 7, 0, 7, 2, 2
+  >>> _box_vote(np.array([[2, -1, 0], [-4, 2, -1], [2, 0, 2], [-2, -8, -10], [2, 2, 1], [-1, 2, -10], [-2, 4, -9]]))
+  (2, 7)
+  '''
+
   size, dim = datapoints.shape
   box = np.zeros(2 ** dim, np.int)
 
-  for raw in (datapoints > 0):
+  for raw in (datapoints >= 0):
     box[_bin2dec(raw)] += 1
 
   box = list(enumerate(box))
@@ -57,11 +93,25 @@ def _box_vote(datapoints):
 
 
 def _bin2coeff(data):
+  '''
+  :input: data <- list
+  :output: data replace 0 to -1 <- list
+  >>> _bin2coeff(np.array([1, 0, 1, 1, 0, 0]))
+  array([ 1, -1,  1,  1, -1, -1])
+  '''
   data[data == 0] = -1
   return data
 
 
 def _boxnum2coeff(dec, dim):
+  ''' translate box id to quadrant representation
+  :input: box id(dec) <- int, dim
+  :output: quadrant <- list
+  >>> _boxnum2coeff(4, 5)
+  array([-1, -1,  1, -1, -1])
+  >>> _boxnum2coeff(11, 7)
+  array([ 1,  1, -1,  1, -1, -1, -1])
+  '''
   bin_num = _dec2bin(dec, dim)
   bin_num[bin_num == 0] = -1
 
@@ -72,11 +122,10 @@ def _boxnum2coeff(dec, dim):
 assume that PA always has higher possibility
 
 """
-def tcf(datapoints, table=None, method=3):
+def _tcf(datapoints, table=None, method=3):
   size, dim = datapoints.shape
   center = np.mean(datapoints, axis=0)
  
-  utl.pprint(datapoints - center) 
   pri_num, sec_num = _box_vote(datapoints - center)
 
   # method 3
@@ -91,28 +140,55 @@ def tcf(datapoints, table=None, method=3):
   centroid_a = _boxnum2coeff(pri_num, dim) * val + center
   centroid_b = _boxnum2coeff(sec_num, dim) * val + center
 
-  print(datapoints)
-  print(centroid_a)
-  print(centroid_b)
-
   return centroid_a, centroid_b
 
 
+"""
+input: datapoints, table, type of method
+output: two list of data in each cluster
+"""
+@utl.log_msg
+def tcf_cut(datapoints, table=None, method=3):
+  centroid_a, centroid_b = _tcf(datapoints, table, method)
+  cluster_a = []
+  cluster_b = []
+
+  for point in datapoints:
+    dist_ca = sum((point - centroid_a) ** 2)
+    dist_cb = sum((point - centroid_b) ** 2)
+    if dist_ca <= dist_cb:
+      cluster_a.append(point)
+    else:
+      cluster_b.append(point)
+
+  return np.array(cluster_a, np.float), np.array(cluster_b, np.float)
+
+
 if __name__ == '__main__':
-  points, label = utl.gaussian_data_generator(dim=2, cls=2, objs_size=[200, 100])
+  doctest.testmod()
+  points, label = utl.gaussian_data_generator(dim=3, cls=5)
+  # _box_vote(np.array([2, -1], [4, 2], [0, 2], [-2, -10], [2, 1]))
+  print("points[0] = ", points[0])
+  _box_vote(points[0:2])
+  # _box_vote(points)
+  # for i in range(2, 26):
+  #   points, label = utl.gaussian_data_generator(dim=i, cls=5, objs_size=[1000, 1000, 1000, 1000, 1000])
+  #   print("dim = ", i)
+  #   tcf_cut(points)
 
-  print(label)
-  for i in np.unique(label):
-    fetch_cluster = points[label == i]
-    plt.scatter(fetch_cluster[:, 0], fetch_cluster[:, 1], color=np.random.rand(3));
+  # a, b = tcf_cut(points)
 
-  table = build_table(range(9, 12))
-  ca, cb = tcf(points, table)
 
-  ca = ca.astype(np.int)
-  cb = cb.astype(np.int)
-
-  plt.plot(ca[0], ca[1], "ro", ms=20)
-  plt.plot(cb[0], cb[1], "go", ms=20)
-
-  plt.show()
+  # print(label)
+  # for i in np.unique(label):
+  #   fetch_cluster = points[label == i]
+  #   plt.scatter(fetch_cluster[:, 0], fetch_cluster[:, 1], color=np.random.rand(3));
+  #
+  # table = build_table(range(9, 12))
+  # ca, cb = _tcf(points, table)
+  #
+  #
+  # plt.plot(ca[0], ca[1], "ro", ms=20)
+  # plt.plot(cb[0], cb[1], "go", ms=20)
+  #
+  # plt.show()
